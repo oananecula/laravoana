d15497031afdcba04bce236fd299f83822185b98
Unknown <necumodi@gmail.com>
2020-02-10 12:32:05 +0200

    test

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -20,6 +20,7 @@
 		</div>
 	</div>
 </div>
+<h1>mere</h1>
 <div id="copyright" class="container">
 	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
 </div>
